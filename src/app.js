7b5e673073bfec9043b39325721cb3bc3f2796a3
André Gonçalves <andgcv@gmail.com>
2019-11-09 19:30:16 +0000

    Refactored endpoints to use async await

--- src/app.js
+++ src/app.js
@@ -10,55 +10,69 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 
 // CREATE User
-app.post('/users', (req, res) => {
+app.post('/users', async (req, res) => {
     const user = new User(req.body)
-    user.save()
-        .then(() => res.status(201).send(user))
-        .catch((e) => res.status(400).send(e))
+    try {
+        await user.save()
+        res.status(201).send(user)
+    } catch (e) {
+        res.status(400).send(e)
+    }
 })
 
 // READ Users
-app.get('/users', (req, res) => {
-    User.find({})
-        .then((users) => res.send(users))
-        .catch((e) => res.status(500).send(e))
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.find({})
+        res.send(users)
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 // READ User by ID
-app.get('/users/:id', (req, res) => {
+app.get('/users/:id', async (req, res) => {
     const _id = req.params.id
-    User.findById(_id)
-        .then((user) => {
-            if (!user) return res.status(404).send('Unable to find user')
-            res.send(user)
-        })
-        .catch((e) => res.status(500).send(e))
+    try {
+        const user = await User.findById(_id)
+        if (!user) return res.status(404).send('Unable to find user')
+        res.send(user)
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 // CREATE Task
-app.post('/tasks', (req, res) => {
+app.post('/tasks', async (req, res) => {
     const task = new Task(req.body)
-    task.save()
-        .then(() => res.status(201).send(task))
-        .catch((e) => res.status(400).send(e))
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 // READ Tasks
-app.get('/tasks', (req, res) => {
-    Task.find({})
-        .then((tasks) => res.send(tasks))
-        .catch((e) => res.status(500).send(e))
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 // READ Task by ID
-app.get('/tasks/:id', (req, res) => {
+app.get('/tasks/:id', async (req, res) => {
     const _id = req.params.id
-    Task.findById(_id)
-        .then((task) => {
-            if (!task) return res.status(404).send('Unable to find task')
-            res.send(task)
-        })
-        .catch((e) => res.status(500).send(e))
+    try {
+        const task = await Task.findById(_id)
+        if (!task) return res.status(404).send('Unable to find task')
+        res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
+    }
 })
 
 app.listen(port, () => {
