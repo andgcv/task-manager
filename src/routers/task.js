72bd1bb83b149a6bbdb0b610192688f899e29447
André Gonçalves <andgcv@gmail.com>
2019-11-16 11:24:35 +0000

    Added authentication to UPDATE Task by ID and DELETE Task by ID

--- src/routers/task.js
+++ src/routers/task.js
@@ -39,8 +39,8 @@ router.get('/tasks/:id', auth, async (req, res) => {
     }
 })
 
-// UPDATE Task by ID
-router.patch('/tasks/:id', async (req, res) => {
+// UPDATE Task by ID of currently authenticated User
+router.patch('/tasks/:id', auth, async (req, res) => {
     // Returns an array of strings with our request body keys
     const updates = Object.keys(req.body)
     const allowedUpdates = ['description', 'completed']
@@ -51,21 +51,21 @@ router.patch('/tasks/:id', async (req, res) => {
 
     try {
         // Find the task by the id we passed in
-        const task = await Task.findById(req.params.id)
+        const task = await Task.findOne({ _id: req.params.id, owner: req.user._id })
+        if (!task) return res.status(404).send('Unable to find task')
         // Update each value individually
         updates.forEach((update) => task[update] = req.body[update])
         await task.save()
-        if (!task) return res.status(404).send('Unable to find task')
         res.send(task)
     } catch (e) {
         res.status(400).send(e)
     }
 })
 
-// DELETE Task by ID
-router.delete('/tasks/:id', async (req, res) => {
+// DELETE Task by ID of currently authenticated User
+router.delete('/tasks/:id', auth, async (req, res) => {
     try {
-        const task = await Task.findByIdAndDelete(req.params.id)
+        const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id })
         if (!task) return res.status(404).send('Unable to find task')
         res.send(task)
     } catch (e) {
